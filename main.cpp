6554395a3e923a3811bcb749b7b9e6858cd600ef
lnagua <lnagua@ing.uc3m.es>
2026-02-27 13:05:26 +0100

    creacion clase Processor

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 //
 #include "Device.h"
 #include "Display.h"
+#include "Processor.h"
 #include <iostream>
 using namespace std;
 
@@ -10,5 +11,8 @@ int main() {
     Display d1("Smartphone");
     cout << "Device Name: " << d1.getName() << endl;
     d1.process("Hello, World!");
+    Processor p1("CPU_1");
+    p1.connectTo(d1);
+    p1.process("Hello, World!");
     return 0;
 }
